cd3eee9ce6c8090b5275b46b9e8737f6eadd7b48
amoughnie <amoughnie@hotmail.com>
2016-05-07 23:58:42 -0400

    Added Habit Class
    Added Habit class

---
+++ app/src/main/java/com/example/ayapp/habits/Habit.java
@@ -0,0 +1,59 @@
+package com.example.ayapp.habits;
+
+/**
+ * Created by amoughnie on 07-May-16.
+ */
+public class Habit {
+
+    private String title;// habit title
+    private String description;// habit description
+    private int goal;// number of days in which you want to preform this habit
+    private int id; // used as an index in the database
+
+    public Habit(String title,String description, int goal, int id){
+        this.title = title;
+        this.description = description;
+        this.goal = goal;
+        this.id = id;
+    }
+
+    public Habit(){
+        this.title = "my first habit";
+        this.description = "this is a test habit";
+        this.goal = 5;
+        this.id = 1234;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getGoal() {
+        return goal;
+    }
+
+    public void setGoal(int goal) {
+        this.goal = goal;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
