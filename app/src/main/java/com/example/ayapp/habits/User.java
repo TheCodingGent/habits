64cb24472cb4cbeb3f27e113db32cca5bb95b599
Yasser Ghamlouch <yasser.ghamlouch@gmail.com>
2016-05-08 14:02:40 -0400

    Fixed User.java
    Made some changes and added sections to the User.java class.

--- app/src/main/java/com/example/ayapp/habits/User.java
+++ app/src/main/java/com/example/ayapp/habits/User.java
@@ -1,70 +1,109 @@
 package com.example.ayapp.habits;
 
-import java.util.ArrayList;
-
 /**
  * Created by yasser.ghamlouch on 2016-05-07.
  */
+
 public class User {
-
-
+    /***********************************************************************************************
+     * VARIABLES
+     **********************************************************************************************/
     // Private instance variables
-    private String mUsername;
-    private String mEmail;
-    private String mPassword;
-    private ArrayList<Habit> mHabits;
+    private int id;
+    private int habitsId;
+    private String username;
+    private String email;
+    private String password;
+    private ArrayList<Habit> habits;
 
 
-    public User(String username, String email, String password){
-        this.mUsername = username;
-        this.mEmail = email;
-        this.mPassword = password;
-        this.mHabits = new ArrayList<Habit>();
+    /***********************************************************************************************
+     * CONSTRUCTORS
+     **********************************************************************************************/
+    public User(){
+        this.id = -1;
+        this.username = null;
+        this.email = null;
+        this.password = null;
+        this.habits = null;
+        this.habitsId = -1;
+    };
+
+    public User(int id, String username, String email, String password){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.habits = new ArrayList<Habit>();
     }
 
-    public User(String username, String email, String password, ArrayList<Habit> habits){
-        this.mUsername = username;
-        this.mEmail = email;
-        this.mPassword = password;
-        this.mHabits = habits;
+    public User(int id, String username, String email, String password, ArrayList<Habit> habits){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.habits = habits;
     }
 
-    // Setters and getters
+
+    /***********************************************************************************************
+     * SETTERS AND GETTERS
+     **********************************************************************************************/
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHabitsId() {
+        return habitsId;
+    }
+
+    public void setHabitsId(int habitsId) {
+        this.habitsId = habitsId;
+    }
+
     public String getUsername() {
-        return mUsername;
+        return username;
     }
 
     public void setUsername(String username) {
-        this.mUsername = username;
+        this.username = username;
     }
 
     public String getEmail() {
-        return mEmail;
+        return email;
     }
 
     public void setEmail(String email) {
-        this.mEmail = email;
+        this.email = email;
     }
 
     public String getPassword() {
-        return mPassword;
+        return password;
     }
 
     public void setPassword(String password) {
-        this.mPassword = password;
+        this.password = password;
     }
 
     public ArrayList<Habit> getHabits() {
-        return mHabits;
+        return habits;
     }
 
     public void setHabits(ArrayList<Habit> habits) {
-        this.mHabits = habits;
+        this.habits = habits;
     }
 
+
+    /***********************************************************************************************
+     * UTILITY METHODS
+     **********************************************************************************************/
     @Override
     public String toString() {
-        return "User [Username=" + mUsername + ", Email=" + mEmail + ", Password=" + mPassword
+        return "User [Username=" + username + ", Email=" + email + ", Password=" + password
                 + "]";
     }
 
