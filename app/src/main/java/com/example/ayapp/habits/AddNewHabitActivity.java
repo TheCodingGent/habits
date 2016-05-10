490ae9f5437b085f5474a3030469e0f3825156ff
amoughnie <amoughnie@hotmail.com>
2016-05-10 19:02:08 -0400

    Added Custom Habits
    Added ability to add custom habits to the list.

---
+++ app/src/main/java/com/example/ayapp/habits/AddNewHabitActivity.java
@@ -0,0 +1,44 @@
+package com.example.ayapp.habits;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddNewHabitActivity extends AppCompatActivity {
+
+    /********************************************************************************************
+     * VARIABLES
+     *******************************************************************************************/
+    Intent intent;
+    EditText habitName;
+    EditText habitDescription;
+    EditText habitGoal;
+    Button addHabitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_habit);
+
+        intent = new Intent();
+
+        habitName = (EditText) findViewById(R.id.addHabitNameEditText);
+        habitDescription = (EditText) findViewById(R.id.addDescriptionEditText);
+        habitGoal = (EditText) findViewById(R.id.addGoalEditText);
+        addHabitButton = (Button) findViewById(R.id.addHabitButton);
+
+        addHabitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.putExtra("name", habitName.getText().toString());
+                intent.putExtra("description", habitDescription.getText().toString());
+                intent.putExtra("goal", habitGoal.getText().toString());
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/ayapp/habits/MainActivity.java
+++ app/src/main/java/com/example/ayapp/habits/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ayapp.habits;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -10,24 +11,30 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    /***********************************************************************************************
+     * VARIABLES
+     **********************************************************************************************/
     // Construct the data source
     // TODO: switch this arraylist with user.arraylist
     ArrayList<Habit> arrayOfHabits;
     // Create the adapter to convert the array to views
     HabitListAdapter adapter;
-
     //created habit object for testing
     //TODO: remove before pushing files
     Habit defaultHabit = new Habit("Default Habit","This is how the Habit description is shown",5,1234);
+    Habit habit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
 
+        //List of Habits
         //initialize the ArrayList
         arrayOfHabits = new ArrayList<Habit>();
         //add random habit to arrayOfHabits for testing
@@ -40,16 +47,38 @@ public class MainActivity extends AppCompatActivity {
         ListView habitListView = (ListView) findViewById(R.id.habitListView);
         habitListView.setAdapter(adapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        assert fab != null;
-        fab.setOnClickListener(new View.OnClickListener() {
+        //Add Habit Button
+        FloatingActionButton addNewHabitButton = (FloatingActionButton) findViewById(R.id.addNewHabitButton);
+        assert addNewHabitButton != null;
+        addNewHabitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //used to check to see if the list is refreshing
-                //TODO: fix before pushing files
-                arrayOfHabits.add(defaultHabit);
-                adapter.notifyDataSetChanged();
+                //starting the add new habit activity
+                //note the onActivityResult method adds the habit to the list directly
+                Intent intent = new Intent(MainActivity.this, AddNewHabitActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1){
+            if(resultCode == RESULT_OK){
+                String name = data.getStringExtra("name");
+                String description = data.getStringExtra("description");
+                int goal;
+                try {
+                    goal = Integer.parseInt(data.getStringExtra("goal"));
+                } catch (NumberFormatException nfe) {
+                    goal = 0;
+                }
+
+                habit = new Habit(name,description,goal, 1234);
+                arrayOfHabits.add(habit);
+                adapter.notifyDataSetChanged();
+            }
+        }
     }
 }
